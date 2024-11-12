2400b58c4f073b4ac5ab566e802dc5e3948eafb3
AgneloFH <tech.agnelo@gmail.com>
2024-11-12 18:45:55 -0300

    added files

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,7 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+
+
+//= link views/layouts/v1/devise_layout.css
+//= link views/devise/index.css
