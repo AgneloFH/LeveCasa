3d2be7fdc849744466519e21730ba1524b9dc8ee
AgneloFH <tech.agnelo@gmail.com>
2024-11-13 01:39:59 -0300

    refactoring files

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -6,3 +6,5 @@
 
 //= link views/layouts/v1/devise_layout.css
 //= link views/devise/index.css
+
+//= link views/layouts/v1/real_estate/base.css
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
+  before_validation :generate_unique_code, on: :create
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+
+  validates :code, presence: true, uniqueness: true
+
+  private
+
+  def generate_unique_code
+    loop do
+      self.code = rand(100000..999999)
+      Rails.logger.info "Generated code: #{self.code}"
+      break unless User.exists?(code: code)
+    end
+  end
 end
