1536f0a688452dfb7a2362a6967a2457610aab5c
AgneloFH <tech.agnelo@gmail.com>
2024-11-13 22:45:19 -0300

    updated base:controller

--- app/controllers/concerns/v1/base_real_estate_controller.rb
+++
@@ -1,5 +0,0 @@
-module V1
-  class BaseRealEstateController < ApplicationController
-    layout 'layouts/v1/real_estate/admin'
-  end
-end
---
+++ app/controllers/v1/real_estate/Admin/base_controller.rb
@@ -0,0 +1,9 @@
+module V1
+  module RealEstate
+    module Admin
+      class BaseController < ApplicationController
+        layout 'layouts/v1/real_estate/admin'
+      end
+    end
+  end
+end
